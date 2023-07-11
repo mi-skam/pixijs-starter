315118ca39f55a73ce9d1164e898c278f2577fd7
mi-skam <codes@miskam.xyz>
2023-07-11 16:19:01 +0200

    moving to typescript

--- app.ts
+++ app.ts
@@ -1,22 +1,26 @@
 import { Application } from '@pixi/app';
 import { Sprite } from '@pixi/sprite';
 import { Assets } from '@pixi/assets';
+import { Texture } from 'pixi.js';
 
 let app = new Application({
+  view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
   autoDensity: true,
   resolution: window.devicePixelRatio || 1,
-  backgroundColor: 0x6495ed,
+  backgroundColor: 0xffffff,
   width: 640,
   height: 480,
 });
 
-document.body.appendChild(app.view);
+const texture: Texture = await Assets.load('assets/crab.png');
+const crab: Sprite = new Sprite(texture);
 
-const texture = await Assets.load('assets/crab.png');
-const crab = new Sprite(texture);
+// resize the crab
+crab.width = crab.width / 4;
+crab.height = crab.height / 4;
 
 // position the crab
-crab.x = app.screen.width / 2;
+crab.x = app.screen.width;
 crab.y = app.screen.height / 2;
 
 // rotate around the center
@@ -34,7 +38,7 @@ app.ticker.add((delta) => {
   // Add the delta time to our elapsed time
   elapsed += delta;
   // Update the crab's x position to match the elapsed time
-  crab.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+  crab.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0 + 200;
 
   crab.rotation += 0.0;
 });
